ad2d7c5465844d0e7bcb244fedde817bf66ef5d2
Toslima Khanam <khanam.toslima@gmail.com>
2024-01-07 15:31:14 +0000

    Update get_channel_task_detail.php

--- php/ui/chat/get_channel_task_detail.php
+++ php/ui/chat/get_channel_task_detail.php
@@ -42,26 +42,38 @@
         $results_array = array();
         if ($results->num_rows > 0) {
             while ($row = $results->fetch_array(MYSQLI_ASSOC)) {
-                $cblscheduleno=$row['cblscheduleno'];
-                $rs_progress=get_progress_detail($dbcon, $cblscheduleno);
-                $progress_array = array();
-                if ($rs_progress->num_rows > 0) {
-                    while ($prow = $rs_progress->fetch_array(MYSQLI_ASSOC)) {
-                        $progress_array[]=$prow;
-                    }
-                }
-                $row['progress'] = $progress_array;
-
-                $rs_deadline=get_all_deadlines($dbcon, $cblscheduleno);
-                $deadline_array = array();
-                if ($rs_deadline->num_rows > 0) {
-                    while ($drow = $rs_deadline->fetch_array(MYSQLI_ASSOC)) {
-                        $deadline_array[]=$drow;
-                    }
-                }
-                $row['deadlines'] = $deadline_array;
-
                 $backlogno = $row['backlogno'];
+
+                $rs_cbschedule=get_task_schedule($dbcon, $backlogno);
+                $schedule_array = array();
+                if ($rs_cbschedule->num_rows > 0) {
+                    while ($srow = $rs_cbschedule->fetch_array(MYSQLI_ASSOC)) {
+                        $cblscheduleno=$srow['cblscheduleno'];
+
+                        $rs_progress=get_progress_detail($dbcon, $cblscheduleno);
+                        $progress_array = array();
+                        if ($rs_progress->num_rows > 0) {
+                            while ($prow = $rs_progress->fetch_array(MYSQLI_ASSOC)) {
+                                $progress_array[]=$prow;
+                            }
+                        }
+                        $srow['progress'] = $progress_array;
+
+                        $rs_deadline=get_all_deadlines($dbcon, $cblscheduleno);
+                        $deadline_array = array();
+                        if ($rs_deadline->num_rows > 0) {
+                            while ($drow = $rs_deadline->fetch_array(MYSQLI_ASSOC)) {
+                                $deadline_array[]=$drow;
+                            }
+                        }
+                        $srow['deadlines'] = $deadline_array;
+
+                        $schedule_array[]=$srow;
+                    }
+                }
+
+                $row['progress'] = $schedule_array;
+                
                 $rs_comments=get_sub_comments($dbcon, $backlogno); 
                 $comment_array = array();
                 if ($rs_comments->num_rows > 0) {
@@ -93,6 +105,48 @@
     //asp_channelbacklog(backlogno, channelno, story, points, prioritylevelno, relativepriority, storytypeno, lastupdatetime, userno)
     //asp_cblschedule(cblscheduleno,backlogno,howto,assignedto, assigntime,scheduledate,userno)
     //asp_cblprogress(cblprogressno,cblscheduleno,progresstime,result,wstatusno,userno)
+    
+    function get_channel_task_update($dbcon, $channelno, $pageno, $limit){
+        $startindex=($pageno-1)*$limit;
+        $sql = "SELECT channelno,(SELECT channeltitle FROM msg_channel WHERE channelno=b.channelno) as channeltitle,
+                        b.backlogno,story,storytype, b.lastupdatetime as storytime,
+                        prioritylevelno,(SELECT priorityleveltitle FROM asp_prioritylevel WHERE prioritylevelno=b.prioritylevelno) as priorityleveltitle,
+                        storyphaseno,(SELECT storyphasetitle FROM asp_storyphase WHERE storyphaseno=b.storyphaseno) as storyphasetitle,
+                        relativepriority,
+                        b.userno, CONCAT(firstname,' ',IFNULL(lastname,'')) as postedby, photo_url
+                FROM asp_channelbacklog as b
+                    INNER JOIN hr_user as u ON b.userno=u.userno
+                WHERE parentbacklogno IS NULL AND channelno=?
+                ORDER BY b.backlogno DESC 
+                LIMIT ?,?
+                ";
+        $stmt = $dbcon->prepare($sql);
+        $stmt->bind_param("iii", $channelno,$startindex,$limit);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+
+        return $result;
+    }
+
+    //asp_cblschedule(cblscheduleno,backlogno,howto,assignedto, assigntime,scheduledate,duration,userno)
+    function get_task_schedule($dbcon, $backlogno){
+        $sql = "SELECT  cblscheduleno,howto, assigntime,scheduledate,duration, 
+                        s.assignedto, CONCAT(u.firstname,' ',IFNULL(u.lastname,'')) as assignee, photo_url
+                FROM asp_cblschedule as s
+                    INNER JOIN hr_user as u ON s.assignedto=u.userno
+                WHERE backlogno=?
+                ORDER BY cblscheduleno DESC 
+                ";
+        $stmt = $dbcon->prepare($sql);
+        $stmt->bind_param("i", $backlogno);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+
+        return $result;
+    }
+
     function get_progress_detail($dbcon, $cblscheduleno){
         $sql = "SELECT cblprogressno,cblscheduleno,progresstime,result,
                     wstatusno,(SELECT statustitle FROM asp_workstatus WHERE wstatusno=p.wstatusno) as statustitle,
@@ -124,35 +178,7 @@
 
         return $result;
     }
-
-    function get_channel_task_update($dbcon, $channelno, $pageno, $limit){
-        $startindex=($pageno-1)*$limit;
-        $sql = "SELECT channelno,(SELECT channeltitle FROM msg_channel WHERE channelno=b.channelno) as channeltitle,
-                        b.backlogno,story,storytype, lastupdatetime as storytime,
-                        prioritylevelno,(SELECT priorityleveltitle FROM asp_prioritylevel WHERE prioritylevelno=b.prioritylevelno) as priorityleveltitle,
-                        storyphaseno,(SELECT storyphasetitle FROM asp_storyphase WHERE storyphaseno=b.storyphaseno) as storyphasetitle,
-                        relativepriority,howto,assigntime,scheduledate,duration,
-                        s.cblscheduleno,
-                        b.userno, b.assignedby,b.photo_url,
-                        s.assignedto, (SELECT CONCAT(firstname,' ',IFNULL(lastname,'')) FROM hr_user WHERE userno=s.assignedto) as assignee
-                FROM
-                    (SELECT backlogno, channelno, story, storyphaseno,storytype, prioritylevelno, relativepriority, cb.lastupdatetime, 
-                            u.userno, CONCAT(firstname,' ',IFNULL(lastname,'')) as assignedby, photo_url
-                    FROM asp_channelbacklog as cb
-                        INNER JOIN hr_user as u ON cb.userno=u.userno
-                    WHERE parentbacklogno IS NULL AND channelno=?) as b
-                    LEFT JOIN asp_cblschedule as s ON s.backlogno=b.backlogno
-                ORDER BY b.backlogno DESC, s.cblscheduleno DESC 
-                LIMIT ?,?
-                ";
-        $stmt = $dbcon->prepare($sql);
-        $stmt->bind_param("iii", $channelno,$startindex,$limit);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $stmt->close();
-
-        return $result;
-    }
+    
 
     function get_sub_comments($dbcon, $backlogno){
         $sql = "SELECT backlogno, story, storyphaseno, storytype, b.lastupdatetime, 

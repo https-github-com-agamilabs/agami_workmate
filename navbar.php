25fb3b5e7635d4320fcea3f885a6bf46282cccb3
Shazzad Hossain Mazumder <shmazumder23@gmail.com>
2024-01-09 15:25:38 +0600

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -128,7 +128,7 @@
                             </form>
 
 
-                            <a href="time_keeper_summary.php"><i class="fas fa-chart-bar fa-2x mr-2 text-primary rounded-circle"></i></a>
+                            <a href="time_keeper_summary.php"><i class="fas fa-chart-bar fa-2x mr-3 text-primary rounded-circle"></i></a>
                         </div>
 
                         <div class="widget-content-left d-flex mt-2">

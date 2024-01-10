b45259b0d00a977c61d404c708b39a72c32d6019
partha-p-nath <parthanath95@gmail.com>
2024-01-10 15:42:27 +0600

    Update check_session.php

--- php/ui/login/check_session.php
+++ php/ui/login/check_session.php
@@ -1,45 +1,44 @@
 <?php
 
 if (session_status() == PHP_SESSION_NONE) {
-    session_start();
+	session_start();
 }
 
-if(!isset($_SESSION['cogo_userno'])){
+if (!isset($_SESSION['cogo_userno'])) {
 
-    if($_SERVER['REQUEST_METHOD']=='POST'){
-        $response = array();
-        $response['error'] = true;
-        $response['message'] = "Session timeout exceeded. Please login again.";
-        echo json_encode($response);
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		$response = array();
+		$response['error'] = true;
+		$response['message'] = "Session timeout exceeded. Please login again.";
+		echo json_encode($response);
 		exit();
 	}
 
-	if($_SERVER['REQUEST_METHOD']=='GET'){
-        header("Location:login.php");
+	if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+		header("Location:index.php");
 		exit();
 	}
 
-  exit();
-}else{
-  if(!isset($_SESSION['cogo_ucatno'])){
-      if($_SERVER['REQUEST_METHOD']=='POST'){
-        $response = array();
-        $response['error'] = true;
-        $response['message'] = "Session timeout exceeded. Please login again.";
-        echo json_encode($response);
-		exit();
-	}
+	exit();
+} else {
+	if (!isset($_SESSION['cogo_ucatno'])) {
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			$response = array();
+			$response['error'] = true;
+			$response['message'] = "Session timeout exceeded. Please login again.";
+			echo json_encode($response);
+			exit();
+		}
 
-	if($_SERVER['REQUEST_METHOD']=='GET'){
-        header("Location:login.php");
+		if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+			header("Location:index.php");
+			exit();
+		}
 		exit();
 	}
-    exit();
-  }
 }
 
 $userno = $_SESSION['cogo_userno'];
 $ucatno = $_SESSION['cogo_ucatno'];
 $my_permissionlevel = $_SESSION['cogo_permissionlevel'];
-
 ?>

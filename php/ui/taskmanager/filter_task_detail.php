42870d20ff2945d5ddcf8ab2053779cee8509db6
Toslima Khanam <khanam.toslima@gmail.com>
2024-01-10 22:52:37 +0000

    Update filter_task_detail.php

--- php/ui/taskmanager/filter_task_detail.php
+++ php/ui/taskmanager/filter_task_detail.php
@@ -1,4 +1,8 @@
 <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    
     $base_path = dirname(dirname(dirname(__FILE__)));
     include_once  $base_path."/ui/login/check_session.php";
 

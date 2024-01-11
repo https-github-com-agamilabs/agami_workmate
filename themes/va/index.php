715fbfc763b51eb342c2f2b8822ed0e44eb6cecf
Toslima Khanam <khanam.toslima@gmail.com>
2024-01-11 22:27:25 +0000

    index

--- themes/va/index.php
+++ themes/va/index.php
@@ -1,9 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-?>
-<?php
 $basePath = dirname(dirname(dirname(__FILE__)));
 include_once($basePath . "/configmanager/org_configuration.php");
 if (!defined("DB_USER")) {
@@ -34,13 +29,13 @@ if (!defined("DB_USER")) {
 <body>
 	<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 		<div class="container">
-			<a class="navbar-brand" href="index.html">Virtual Assistant</a>
+			<a class="navbar-brand" href="index.php">Virtual Assistant</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="oi oi-menu"></span> Menu
 			</button>
 			<div class="collapse navbar-collapse" id="ftco-nav">
 				<ul class="navbar-nav ml-auto">
-					<li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
+					<li class="nav-item active"><a href="index.php" class="nav-link">Home</a></li>
 					<li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
 					<li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
 					<li class="nav-item"><a href="how-it-works.html" class="nav-link">How It Works</a></li>

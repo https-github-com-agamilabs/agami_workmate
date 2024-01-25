c4a7162b7aa1cb17f97de144924d25e4f0148149
Shazzad Hossain Mazumder <shmazumder23@gmail.com>
2024-01-25 10:38:17 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php 
-include_once dirname(__FILE__)."/themes/default/index.php";
+// // default theme
+// include_once dirname(__FILE__)."/themes/default/index.php";
 
 // // new theme
 // include_once dirname(__FILE__)."/themes/va/index.php";
+
+// // import smartaccounting theme
+include_once dirname(__FILE__)."/themes/medi/index.php";
  ?>

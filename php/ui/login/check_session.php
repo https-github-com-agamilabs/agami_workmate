8b9c1c68fe2d3ef37858c9421a168ebe255669fb
Toslima Khanam <khanam.toslima@gmail.com>
2024-02-03 12:27:48 +0000

    Update check_session.php

--- php/ui/login/check_session.php
+++ php/ui/login/check_session.php
@@ -20,25 +20,9 @@ if (!isset($_SESSION['cogo_userno'])) {
 	}
 
 	exit();
-} else {
-	// if (!isset($_SESSION['cogo_ucatno'])) {
-	// 	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	// 		$response = array();
-	// 		$response['error'] = true;
-	// 		$response['message'] = "Session timeout exceeded. Please login again.";
-	// 		echo json_encode($response);
-	// 		exit();
-	// 	}
+} 
 
-	// 	if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-	// 		header("Location:index.php");
-	// 		exit();
-	// 	}
-	// 	exit();
-	// }
-}
 
 $userno = $_SESSION['cogo_userno'];
-$ucatno = $_SESSION['cogo_ucatno'];
-$my_permissionlevel = $_SESSION['cogo_permissionlevel'];
+
 ?>

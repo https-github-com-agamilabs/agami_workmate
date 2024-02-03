4b90048b020c8da6f4f1f7b10bb2721718208e7d
Toslima Khanam <khanam.toslima@gmail.com>
2024-02-03 14:26:45 +0000

    Create get_userorg_detail.php

---
+++ php/ui/organization/get_userorg_detail.php
@@ -0,0 +1,70 @@
+<?php
+include_once  dirname(dirname(__FILE__)) . "/login/check_session.php";
+?>
+<?php
+$response = array();
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    $response['error'] = true;
+    $response['message'] = "Invalid Request method";
+    echo json_encode($response);
+    exit();
+}
+
+$base_path = dirname(dirname(dirname(__FILE__)));
+require_once dirname(dirname(__FILE__)) . "/dependency_checker.php";
+
+try {
+
+    if (isset($_POST['orgno']) && strlen($_POST['orgno']) > 0) {
+        $orgno = (int) $_POST['orgno'];
+    } else {
+        throw new Exception("You must select an organization!", 1);
+    }
+    $result = get_userorgs($dbcon, $orgno);
+
+    $meta_array = array();
+    if ($result->num_rows > 0) {
+        $response['error'] = false;
+        while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+            $meta_array[] = $row;
+        }
+
+        $response['results'] = $meta_array;
+    } else {
+        $response['error'] = true;
+        $response['message'] = "No User at the Organization Found!";
+    }
+} catch (Exception $e) {
+    $response['error'] = true;
+    $response['message'] = $e->getMessage();
+}
+
+echo json_encode($response);
+$dbcon->close();
+
+//com_userorg (uono,orgno,userno,uuid,ucatno,supervisor,moduleno,jobtitle,hourlyrate,monthlysalary,permissionlevel,dailyworkinghour,timeflexibility,shiftno,starttime,endtime,isactive)
+//hr_user(userno,username,firstname,lastname,affiliation,jobtitle,email,primarycontact,passphrase,authkey,ucatno,supervisor,permissionlevel,createtime,lastupdatetime,isactive,userstatusno)
+//com_modules(moduleno,moduletitle)
+function get_userorgs($dbcon, $orgno)
+{
+    $sql = "SELECT uono,uo.orgno,
+                    userno,uuid,ucatno,supervisor,
+                    uo.moduleno,(SELECT moduletitle FROM com_modules WHERE moduleno=uo.moduleno) as moduletitle,
+                    jobtitle,hourlyrate,monthlysalary,permissionlevel,dailyworkinghour,timeflexibility,shiftno,
+                    uo.starttime,uo.endtime,uo.isactive,
+                    u.username,u.firstname,u.lastname,u.affiliation,u.email,u.primarycontact,u.userstatusno
+            FROM com_userorg AS uo
+                INNER JOIN hr_user as u ON u.userno=uo.userno
+            WHERE uo.orgno=?";
+
+    $stmt = $dbcon->prepare($sql);
+    $stmt->bind_param("i", $orgno);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
+    $stmt->close();
+
+    return $result;
+}
+?>

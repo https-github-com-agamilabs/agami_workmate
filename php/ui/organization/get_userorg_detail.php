6bc6cba6ad2e11392ef8143b8f50cafcafaf740c
Toslima Khanam <khanam.toslima@gmail.com>
2024-02-03 14:41:34 +0000

    Update get_userorg_detail.php

--- php/ui/organization/get_userorg_detail.php
+++ php/ui/organization/get_userorg_detail.php
@@ -48,7 +48,7 @@ $dbcon->close();
 function get_userorgs($dbcon, $orgno)
 {
     $sql = "SELECT uono,uo.orgno,
-                    userno,uuid,ucatno,supervisor,
+                    uo.userno,uo.uuid,uo.ucatno,uo.supervisor,
                     uo.moduleno,(SELECT moduletitle FROM com_modules WHERE moduleno=uo.moduleno) as moduletitle,
                     jobtitle,hourlyrate,monthlysalary,permissionlevel,dailyworkinghour,timeflexibility,shiftno,
                     uo.starttime,uo.endtime,uo.isactive,

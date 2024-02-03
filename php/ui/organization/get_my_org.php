039e4f15ab5ebfb215b4b57719ca441a02b90f1f
Toslima Khanam <khanam.toslima@gmail.com>
2024-02-03 12:43:46 +0000

    Update get_my_org.php

--- php/ui/organization/get_my_org.php
+++ php/ui/organization/get_my_org.php
@@ -21,16 +21,7 @@ try {
         $response['error'] = false;
         while ($row = $rs_org->fetch_array(MYSQLI_ASSOC)) {
             $orgno = $row['orgno'];
-
-            $rs_userrole = get_org_userrole($dbcon, $orgno, $userno);
-            $roleArray = array();
-            if ($rs_userrole->num_rows > 0) {
-                while ($urow = $rs_userrole->fetch_array(MYSQLI_ASSOC)) {
-                    $roleArray[] = $urow;
-                }
-            }
-            $row['modules'] = $roleArray;
-
+            
             $rs_validity = check_org_validity($dbcon, $orgno);
             if ($rs_validity->num_rows > 0) {
                 $vrow = $rs_validity->fetch_array(MYSQLI_ASSOC);
@@ -59,47 +50,33 @@ echo json_encode($response);
 $dbcon->close();
 
 //com_orgs (orgno, orgname, street, city, state, country, gpslat, gpslon, orgtypeid, privacy, picurl, primarycontact, orgnote, weekend1, weekend2, starttime, endtime, verifiedno)
-//com_userorgmodules(orgno,userno,moduleno,verified)
+//com_userorg (uono,orgno,userno,uuid,ucatno,supervisor,moduleno,jobtitle,hourlyrate,monthlysalary,permissionlevel,dailyworkinghour,timeflexibility,shiftno,starttime,endtime,isactive)
 //com_modules(moduleno,moduletitle)
 //com_orgtype (orgtypeid,orgtypename,typetag,iconurl)
 //pack_appliedpackage(itemno,orgno,schemeno,appliedat,appliedby,validuntil)
 function get_orgs_of_an_user($dbcon, $userno)
 {
-    $sql = "SELECT o.*
+    $sql = "SELECT 
+                uo.uuid,
+                uo.ucatno,
+                uo.moduleno,(SELECT moduletitle FROM com_modules WHERE moduleno=uo.moduleno) as moduletitle,
+                uo.jobtitle,uo.hourlyrate,uo.monthlysalary,uo.permissionlevel,uo.dailyworkinghour,
+                uo.timeflexibility,
+                uo.shiftno,
+                uo.starttime,uo.endtime,
+                o.orgno, o.orgname, o.street, o.city, o.state, o.country, o.gpslat, o.gpslon, 
+                o.orgtypeid, 
+                o.privacy, 
+                o.picurl, o.primarycontact, o.orgnote, o.weekend1, o.weekend2, o.starttime, o.endtime, verifiedno
             FROM com_orgs as o
-            WHERE o.orgno IN(SELECT DISTINCT orgno
-                             FROM com_userorgmodules
-                             WHERE userno=?)";
+                INNER JOIN (SELECT *
+                             FROM com_userorg
+                             WHERE isactive=1 AND userno=?) as uo
+                ON o.orgno=uo.orgno";
 
     $stmt = $dbcon->prepare($sql);
     if ($stmt) {
         $stmt->bind_param("i", $userno);
-        if ($stmt->execute()) {
-            $result = $stmt->get_result();
-            $stmt->close();
-            return $result;
-        } else {
-            $stmt->close();
-            return false;
-        }
-    } else {
-        return false;
-    }
-}
-
-//com_userorgmodules(orgno,userno,moduleno,isactive)
-//com_modules(moduleno,moduletitle)
-//com_orgtype (orgtypeid,orgtypename,typetag,iconurl)
-//pack_appliedpackage(itemno,orgno,schemeno,appliedat,appliedby,validuntil)
-function get_org_userrole($dbcon, $orgno, $userno)
-{
-    $sql = "SELECT moduleno
-            FROM com_userorgmodules as uo
-            WHERE uo.orgno=? AND uo.userno=? AND isactive=1";
-
-    $stmt = $dbcon->prepare($sql);
-    if ($stmt) {
-        $stmt->bind_param("ii", $orgno, $userno);
         if ($stmt->execute()) {
             $result = $stmt->get_result();
             $stmt->close();

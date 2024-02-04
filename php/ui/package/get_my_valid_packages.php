caedc5168fa3fffbca9986f32993a53b458ebd49
Toslima Khanam <khanam.toslima@gmail.com>
2024-02-04 06:13:41 +0000

    Update get_my_valid_packages.php

--- php/ui/package/get_my_valid_packages.php
+++ php/ui/package/get_my_valid_packages.php
@@ -15,7 +15,13 @@ require_once dirname(dirname(__FILE__)) . "/dependency_checker.php";
 
 try {
 
-    $rs_packages = get_my_package_usability($dbcon,$userno);
+    if (isset($_POST['orgno']) && strlen($_POST['orgno']) > 0) {
+        $orgno = (int) $_POST['orgno'];
+    } else {
+        throw new Exception("Organization must be selected", 1);
+    }
+
+    $rs_packages = get_my_package_usability($dbcon,$userno,$orgno);
 
     if ($rs_packages->num_rows > 0) {
         $meta_array = array();

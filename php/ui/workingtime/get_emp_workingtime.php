efa4e4a2d62286dcce9a77428755d8e4d86e5a31
Shazzad Hossain Mazumder <shmazumder23@gmail.com>
2024-02-06 13:56:01 +0600

    Update get_emp_workingtime.php

--- php/ui/workingtime/get_emp_workingtime.php
+++ php/ui/workingtime/get_emp_workingtime.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
     include_once  dirname(dirname(__FILE__))."/login/check_session.php";
 
     $response = array();

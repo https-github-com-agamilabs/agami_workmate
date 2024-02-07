682565a3faaeeef68fec01b7746f60c100cb26fe
Toslima Khanam <khanam.toslima@gmail.com>
2024-02-07 11:59:44 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php 
-// // default theme
-// include_once dirname(__FILE__)."/themes/default/index.php";
-
-// // new theme
-// include_once dirname(__FILE__)."/themes/va/index.php";
-
-// // import smarthiworkmate theme
-include_once dirname(__FILE__)."/themes/smart/index.php";
+    // import smarthiworkmate theme
+    include_once dirname(__FILE__)."/themes/smart/index.php";
  ?>

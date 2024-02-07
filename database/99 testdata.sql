c1f5a5ebbb67b6d2567594013e9607f07af55b2d
Toslima Khanam <khanam.toslima@gmail.com>
2024-02-07 11:59:36 +0000

    Update 99 testdata.sql

--- database/99 testdata.sql
+++ database/99 testdata.sql
@@ -3,9 +3,6 @@ INSERT INTO hr_user(userno,username,firstname,lastname,email,primarycontact,pass
 (2,'agamiadmin','AGAMi','Admin','agamilabs@gmail.com','+880 1928718272','$2y$10$zwz0EaC8gN1oLJZN68I.2OeCspGusUqJKrkAjPqUcHezW/rm2y2wu',1,7);
 
 
-INSERT INTO com_orgs (orgno, orgname, street, city, state, country, gpslat, gpslon, orgtypeid, privacy, picurl, primarycontact, orgnote, weekend1, weekend2, starttime, endtime, verifiedno) VALUES
-(1, 'AGAMiLabs', 'NK Bhaban, CU Road #1, Hathazari', 'Chattogram', 'Chattogram', 'Bangladesh', '22.4741655', '91.8079191', 60, 2, 'agami_logo.png', '01711308141', 'Note here', NULL, NULL, '08:00:00', '22:00:00', 1);
-
 INSERT INTO com_userorg (uono,orgno,userno,uuid,ucatno,moduleno,designation,permissionlevel,timezone,isactive) VALUES
 (1,1,1,'Agami!Com@Bd1',19,1,'Agamian',1,'Asia/Dhaka',1),
 (2,1,2,'Agami!Com@Bd2',19,1,'Owner',1,'Asia/Dhaka',1);

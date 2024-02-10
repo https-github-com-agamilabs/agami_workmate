274268563ac44fb7ae32d7498c73ad02a69f06de
Toslima Khanam <khanam.toslima@gmail.com>
2024-02-10 18:44:39 +0000

    Update get_user_wherework_today.php

--- php/ui/userattlocset/get_user_wherework_today.php
+++ php/ui/userattlocset/get_user_wherework_today.php
@@ -42,14 +42,16 @@ try {
 echo json_encode($response);
 $dbcon->close();
 
-//com_userattlocset (attlocno,orgno,userno, loclat, loclon,starttime,endtime)
+//com_userattlocset (attlocno,orgno,userno, locno,mindistance,starttime,endtime)
+//com_workinglocation(locno,locname,loclat,loclon,active)
 function get_user_wherework($dbcon, $orgno,$userno)
 {
-    $sql = "SELECT loclat, loclon,starttime,endtime
-            FROM com_userattlocset
-            WHERE orgno=? 
-                AND userno =?
-                AND (CURRENT_DATE() BETWEEN DATE(starttime) AND DATE(DATE_ADD(starttime, INTERVAL duration DAY)))
+    $sql = "SELECT wl.loclat, wl.loclon,uls.starttime,uls.endtime
+            FROM com_userattlocset as uls
+                INNER JOIN com_workinglocation as wl ON uls.locno=wl.locno
+            WHERE uls.orgno=? 
+                AND uls.userno =?
+                AND (NOW() BETWEEN starttime AND endtime)
             ";
 
     $stmt = $dbcon->prepare($sql);

1e571ce26344e6ea98e21c9691488cdb0049aa62
Toslima Khanam <khanam.toslima@gmail.com>
2024-02-10 19:01:16 +0000

    Create get_workinglocation.php

---
+++ php/ui/workinglocation/get_workinglocation.php
@@ -0,0 +1,60 @@
+<?php
+$base_path = dirname(dirname(dirname(__FILE__)));
+include_once($base_path . "/ui/login/check_session.php");
+
+$response = array();
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    $response['error'] = true;
+    $response['message'] = "Invalid Request method";
+    echo json_encode($response);
+    exit();
+}
+
+require_once dirname(dirname(__FILE__)) . "/dependency_checker.php";
+
+
+try {
+    if (isset($_POST['orgno']) && strlen($_POST['orgno']) > 0) {
+        $orgno = (int) $_POST['orgno'];
+    } else {
+        if (!isset($orgno) || strlen($orgno) <= 0) {
+            throw new Exception("Organization must be selected!!", 1);
+        }
+    }
+
+    $rs_workinglocation = get_workinglocation($dbcon, $orgno);
+
+    if ($rs_workinglocation->num_rows > 0) {
+        $meta_array = array();
+        while ($row = $rs_workinglocation->fetch_array(MYSQLI_ASSOC)) {
+            $meta_array[] = $row;
+        }
+        $response['error'] = false;
+        $response['results'] = $meta_array;
+    } else {
+        throw new \Exception("No Data Found!", 1);
+    }
+} catch (Exception $e) {
+    $response['error'] = true;
+    $response['message'] = $e->getMessage();
+}
+
+echo json_encode($response);
+$dbcon->close();
+
+//com_workinglocation(locno,orgno,locname,loclat,loclon,active)
+function get_workinglocation($dbcon, $orgno)
+{
+    $sql = "SELECT locno,locname, loclat,loclon
+            FROM com_workinglocation
+            WHERE orgno=? 
+                AND active=1
+            ";
+
+    $stmt = $dbcon->prepare($sql);
+    $stmt->bind_param("i", $orgno);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $stmt->close();
+    return $result;
+}

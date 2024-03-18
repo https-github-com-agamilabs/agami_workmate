07cc4cd4bee3b596b6a1bad0fac559ba411dadff
Toslima Khanam <khanam.toslima@gmail.com>
2024-03-18 00:02:31 +0000

    Update setup_schedule.php

--- php/ui/taskmanager/schedule/setup_schedule.php
+++ php/ui/taskmanager/schedule/setup_schedule.php
@@ -109,8 +109,25 @@
 
                 $wno=insert_watchlist($dbcon, $assignedto,$backlogno, $orgno);
 
+
+                $rs_to = get_userinfo($dbcon,$assignedto);
+                if($rs_to->num_rows>0){
+                    $trow = $rs_to->fetch_array(MYSQLI_ASSOC);
+                    $to=$trow['email'];
+                    $greet='Hi '.$trow['firstname'].' '.(isset($trow['lastname'])?$trow['lastname']:'');
+                    
+                    $from = "info@agamilabs.com";
+                    $cc =  "khanam.toslima@gmail.com";
+                    $message=build_message($dbcon, $backlogno, $howto, $scheduledate, $duration, $greet);
+                    $is_sent=sendMail($from, $to, $cc, $subject, $message);
+                }
+
                 $response['error'] = false;
-                $response['message'] = "Schedule is Successfully Added.";
+                if($is_sent){
+                    $response['message'] = "Schedule is Successfully added and email is sent.";
+                }else{
+                    $response['message'] = "Schedule is Successfully added";
+                }
             }else{
                 $dbcon->rollback();
                 throw new \Exception("Cannot Add Schedule.", 1);
@@ -214,3 +231,68 @@
         return $stmt->affected_rows;
     }
 
+    //hr_user(userno,username,firstname,lastname,affiliation,jobtitle,photo_url,email,primarycontact,passphrase,authkey,createtime,lastupdatetime,isactive,userstatusno)
+    function get_userinfo($dbcon,$userno){
+        $sql = "SELECT firstname,lastname,email
+                FROM hr_user
+                WHERE userno=?";
+        $stmt = $dbcon->prepare($sql);
+        $stmt->bind_param("i", $userno);
+        $stmt->execute();
+        return $stmt->get_result();
+    }
+
+    //asp_channelbacklog(backlogno,channelno,story,storytype,prioritylevelno,relativepriority,storyphaseno,parentbacklogno,approved,accessibility,lastupdatetime,userno)
+    //asp_prioritylevel(prioritylevelno,priorityleveltitle,priorityleveldescription, colorno)
+    function build_message($dbcon, $backlogno, $howto, $scheduledate, $duration, $greet){
+        $sql = "SELECT story,
+                        prioritylevelno,(SELECT priorityleveltitle FROM asp_prioritylevel WHERE prioritylevelno=b.prioritylevelno) as priorityleveltitle
+                FROM asp_channelbacklog as b
+                WHERE backlogno=?";
+        $stmt = $dbcon->prepare($sql);
+        $stmt->bind_param("i", $backlogno);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+
+        $message = $greet.",<br>The following task has been assigned to you:<br>";
+        if($result->num_rows>0){
+            $srow = $result->fetch_array(MYSQLI_ASSOC);
+            $story=$srow['story'];
+            $priority=$srow['priorityleveltitle'];
+            
+            $message .= "Task: ".$story."br>";
+            $message .= "Priority: ".$priority."<br>";
+            $message .= isset($howto)?"How-to: ".$howto."<br>":"";
+            $message .= "Start-date: ".$scheduledate."<br>";
+            $message .= "Deadline: In ".$duration." day(s). <br>";
+
+            $message .= "<br><br>Your task may affect our customer\'s expectation. <br><br>Therefore, you are obliged to inform early with explanation if you cannot complete the task in-time.<br>We appreciate your understanding."
+            $message .= "<br><br>With best wishes,<br>AGAMiLabs Limited.";
+        }
+    }
+
+    function sendMail($from, $to, $cc, $subject, $message){
+        //Validated
+        if (!isset($from) || !validateEmail($from)) return false;
+        if (!isset($to) || !validateEmail($to)) return false;
+        if (!isset($subject)) $subject = "A task is assigned to you from Workmate";
+
+        if (!isset($message)) {
+            $message = "Please login workmate to check your task in detail.";
+        }
+
+        // Always set content-type when sending HTML email
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+        // More headers
+        $headers .= 'From: ' . $from . "\r\n";
+        if (isset($cc) && strlen($cc)>0) {
+            $headers .= 'Cc: ' . $cc . "\r\n";
+        }
+
+        return mail($to, $subject, $message, $headers);
+    }
+
+    

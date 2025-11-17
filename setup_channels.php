582b3f3d3141a970137841d8448ecaa5648dabfd
Shazzad Hossain Mazumder <shmazumder23@gmail.com>
2025-11-17 14:41:06 +0600

    up

--- setup_channels.php
+++ setup_channels.php
@@ -230,15 +230,16 @@
 			$.each(data, (index, value) => {
 				let slCell = $("<td>").append($(`#channel_table_tbody tr`).length + 1);
 				let parentchannelCell = $("<td>").append(parentchannel || "-");
-				let channeltitleCell = $("<td>").append(`
-					<div style='white-space:wrap;'>
-						<i class='fa ${value.isactive==0?'fa-lock text-secondary':value.isactive<0?'fa-archive text-warning':'fa-check-circle text-success'} mr-1'></i>
-						${value.channeltitle || "-"}
-					</div>
-				`);
+				let channeltitleCell = $("<td>");
+				let channeltitleDiv = $("<div>")
+				.attr('style', 'display: flex; align-items: center;white-space:no-wrap;')
+				.append(`
+					<i class='fa ${value.isactive==0?'fa-lock text-secondary':value.isactive<0?'fa-archive text-warning':'fa-check-circle text-success'} mr-1'></i>
+					<div style='max-width: 30vw;'>${value.channeltitle}</div>
+				`).appendTo(channeltitleCell);
 
 				let editButton = $("<i>")
-					.appendTo(channeltitleCell)
+					.appendTo(channeltitleDiv)
 					.attr({
 						"class": "fas fa-pen text-info mx-1 ml-3",
 						"type": "button",
@@ -246,7 +247,7 @@
 					});
 
 				let deleteButton = $("<i>")
-					.appendTo(channeltitleCell)
+					.appendTo(channeltitleDiv)
 					.attr({
 						"class": "fas fa-times text-danger mx-1 ml-1",
 						"type": "button",

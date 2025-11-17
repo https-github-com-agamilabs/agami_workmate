1b7da421fbce416f5200ddc1203cfb1f88bb19a2
Shazzad Hossain Mazumder <shmazumder23@gmail.com>
2025-11-17 12:45:21 +0600

    up

--- php/ui/cron/task_reminder_cron.php
+++ php/ui/cron/task_reminder_cron.php
@@ -10,7 +10,10 @@
  *  - HTML + WhatsApp
  */
 
-ini_set('display_errors', '0');
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 date_default_timezone_set('Asia/Dhaka');
 
 $now = new DateTime();
